bdc9820054116edcd2dfec66e0cb5245c08c85d9
Emmanuel <ema2159@gmail.com>
2018-10-14 13:29:01 -0800

    Creada actividad de restaurantes

--- app/src/main/java/example/emmanuel/proyecto2microcompus/MainActivity.java
+++ app/src/main/java/example/emmanuel/proyecto2microcompus/MainActivity.java
@@ -1,13 +1,34 @@
 package example.emmanuel.proyecto2microcompus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button coffbtn, resbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        coffbtn=(Button)findViewById(R.id.coffbutton);
+        coffbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),coffeactivity.class);
+                startActivity(i);
+            }
+        });
+        resbtn=(Button)findViewById(R.id.resbutton);
+        resbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),resActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
